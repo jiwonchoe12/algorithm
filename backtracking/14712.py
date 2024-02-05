1b6deb7c20e2daf8f7b0da47c8957b53f2296991
Jiwon Choe <jichoe@c6r2s8.42seoul.kr>
2024-02-05 16:14:09 +0900

    2/5

--- backtracking/14712.py
+++ backtracking/14712.py
@@ -1,10 +1,27 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-if (N == 1 or M == 1):
-	print(2 ** max(N, M))
-else:
-	case = N - 1 + M - 2
-	choice = N * M - 4
-	square = case #2x2 정사각형만 선택
-	for i in range(choice): #정사각형 이외에 선택할 수 있는 개수
+arr = [[0 for i in range(M)] for j in range(N)]
+
+cnt = 0
+
+def can_check(i, j):
+	if (i == 0 or j == 0):
+		return (True)
+	if (arr[i][j-1] == 1 and arr[i-1][j] == 1 and arr[i-1][j-1] == 1):
+		return (False)
+	return (True)
+
+def dfs(idx):
+	global cnt
+	if (idx == N*M):
+		cnt += 1
+	else:
+		if (can_check(idx//M, idx%M)):
+			arr[idx//M][idx%M] = 1
+			dfs(idx+1)
+			arr[idx//M][idx%M] = 0
+		dfs(idx+1)
+
+dfs(0)
+print(cnt)
